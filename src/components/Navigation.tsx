ab9344d6a10ec91d22d58be2a4e5d02e02b79c21
Dominik Dudek <dom-dud774@o2.pl>
2025-07-23 16:51:06 +0200

    Feat: changes in contributor page, created component for navigation

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ContainerBig from "./ContainerBig";
+
+const Navigation: React.FC = () => {
+  return (
+    <ContainerBig>
+        <nav className="flex justify-between">
+            <img src="" alt="" />
+
+        </nav>
+    </ContainerBig>
+  );
+};
+
+export default Navigation;
