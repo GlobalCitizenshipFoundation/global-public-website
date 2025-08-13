1934f4ddaa3a61a6c8f23f47409cfd1b72855560
Dominik Dudek <dom-dud774@o2.pl>
2025-08-13 20:23:57 +0200

    netlify fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
+const path = require("path")
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.module.rules.push({
+        test: /\.ts$/,
+        include: path.resolve(__dirname, "cms"),
+        loader: "ignore-loader",
+      });
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
