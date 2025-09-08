54fd313bf99e8e204863e26ee4d6f46d2672c98e
Mateusz Cieplak <m.cieplak97@gmail.com>
2025-09-08 02:16:37 +0200

    fix: added magazinSingle to schemaTypes

--- cms/schemaTypes/index.ts
+++ cms/schemaTypes/index.ts
@@ -2,5 +2,6 @@ import {article} from './article'
 import {contributorSingle} from './contributorSingle'
 import {eventSingle} from './eventSingle'
 import {partnerSingle} from './partnerSingle'
+import {magazinSingle} from './magazinSingle'
 
-export const schemaTypes = [article, eventSingle, contributorSingle, partnerSingle]
+export const schemaTypes = [article, eventSingle, contributorSingle, partnerSingle, magazinSingle]
