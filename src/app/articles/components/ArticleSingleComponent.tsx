8c1d5ec6380b21a35cb6454ea8db804e4c79acb1
Bartek <kowalski_1410@wp.pl>
2026-01-20 13:50:31 +0100

    add sidebar on page

--- src/app/articles/components/ArticleSingleComponent.tsx
+++ src/app/articles/components/ArticleSingleComponent.tsx
@@ -29,6 +29,34 @@ const ArticleSingleComponent: React.FC<Props> = ({ article }) => {
     },
   };
 
+  const sidebarPortableText = {
+    block: {
+      normal: ({ children }: PortableTextComponentProps<PortableTextBlock>) => (
+        <p className="text-body text-sm [overflow-wrap:anywhere] break-words whitespace-normal">
+          {children}
+        </p>
+      ),
+    },
+  };
+
+  const authorsTemp = [
+    {
+      name: 'Moana',
+      description: 'Designation, University of Educational Sciences, Country Name',
+      imgUrl: '/images/author1.png',
+    },
+    {
+      name: 'Teressa',
+      description: 'Designation Professional, University of Leadership Sciences, Residence Nation',
+      imgUrl: '/images/author2.png',
+    },
+    {
+      name: 'Lissa William',
+      description: 'Designation, University of Educational Sciences, Country of Residence',
+      imgUrl: '/images/author3.png',
+    },
+  ];
+
   return (
     <article className="bg-background-primary w-full overflow-x-hidden">
       <ContainerBig>
@@ -89,11 +117,118 @@ const ArticleSingleComponent: React.FC<Props> = ({ article }) => {
                 )}
               </ContainerRegular>
             </div>
-            <aside className="min-w-0 space-y-6">
-              <ContainerRegular className="min-w-0 text-sm lg:text-base">
-                <div>aaa</div>
-                aaa
-              </ContainerRegular>
+            <aside className="min-w-0 space-y-12">
+              <section className="min-w-0 text-sm lg:text-base">
+                <div className="inline-flex space-x-2 rounded-[6px] border border-black px-2 py-1">
+                  {article.readingLength && (
+                    <>
+                      <img src="/images/time.svg" alt="" />
+                      <span>
+                        {article.readingLength} {article.readingLength > 1 ? 'minutes' : 'minute'}{' '}
+                        {'Read'}
+                      </span>
+                    </>
+                  )}
+                </div>
+              </section>
+              <section className="min-w-0 text-sm lg:text-base">
+                <div className="space-y-4">
+                  {article.authors.length > 0 && <h3 className="mb-4">Authors</h3>}
+                  {article.authors &&
+                    authorsTemp.map((author) => (
+                      <div key={author.imgUrl} className="flex flex-row items-center gap-x-4">
+                        <img
+                          src={author.imgUrl}
+                          alt={author.name}
+                          className="h-[70px] w-[70px] rounded-full"
+                        />
+                        <div className="flex flex-col">
+                          <span>{author.name}</span>
+                          <span className="text-xs">{author.description}</span>
+                        </div>
+                      </div>
+                    ))}
+                </div>
+              </section>
+              <section className="min-w-0 text-sm lg:text-base">
+                <div>
+                  {article.disclosureStatement && (
+                    <>
+                      <h3 className="mb-4">Disclosure Statement</h3>
+                      <PortableText
+                        value={article.disclosureStatement}
+                        components={sidebarPortableText}
+                      />
+                    </>
+                  )}
+                </div>
+              </section>
+              <section className="min-w-0 text-sm lg:text-base">
+                {article.partners && <h3 className="mb-4">Partners</h3>}
+                {article.partners && (
+                  <div
+                    style={{ backgroundColor: '#D9D9D9' }}
+                    className="mx-auto max-w-4xl rounded-[8px] p-4"
+                  >
+                    {/* Grid kafelków */}
+                    <div className="grid max-w-[calc(2*150px+1rem)] grid-cols-2 gap-4">
+                      {article.partners.slice(0, 4).map((partner) => (
+                        <div
+                          key={partner.title}
+                          className="flex h-20 items-center justify-center rounded-lg bg-white p-4 shadow"
+                        >
+                          <img
+                            src={partner.logo?.asset.url}
+                            alt={partner.title}
+                            className="max-h-full max-w-full object-contain"
+                          />
+                        </div>
+                      ))}
+                    </div>
+
+                    {/* Opis */}
+                    <div className="mt-6 space-y-4 text-center text-sm text-gray-700">
+                      <p>Leadership as the capacity of a human community to shape.</p>
+                      <p>Leadership as the capacity of a human community to shape.</p>
+                    </div>
+
+                    {/* Button */}
+                    <div className="mt-4 text-center">
+                      <button className="rounded bg-black px-6 py-2 text-white">
+                        View All Partners
+                      </button>
+                    </div>
+                  </div>
+                )}
+              </section>
+              <section className="min-w-0 text-sm lg:text-base">
+                <div style={{ backgroundColor: '#D9D9D9' }} className="max-w-sm rounded-lg p-6">
+                  {/* Logo */}
+                  <div className="mb-4">
+                    <img
+                      src="/images/additionalLogo.svg"
+                      alt="Creative Commons"
+                      className="h-10 w-20"
+                    />
+                  </div>
+
+                  {/* Tekst */}
+                  <div className="mb-6">
+                    <h2 className="mb-2 text-lg font-semibold">
+                      We believe in the free flow of information
+                    </h2>
+                    <p className="text-sm text-gray-700">
+                      Republish our articles for free, online or in print, under Creative Commons
+                      licence.
+                    </p>
+                  </div>
+
+                  {/* Button */}
+                  <button className="w-full rounded bg-black px-4 py-2 text-white transition-colors hover:bg-gray-800">
+                    Republish this article
+                  </button>
+                </div>
+              </section>
             </aside>
           </div>
         </section>
