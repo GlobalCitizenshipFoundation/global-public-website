76469fdc7163cc89306180d9ee3d130dbd8b630b
Michał Szwindowski <github@nedi.me>
2026-02-22 13:40:54 +0100

    #3

--- apps/web/src/shared/sanity/server.ts
+++ apps/web/src/shared/sanity/server.ts
@@ -7,9 +7,9 @@ function requireEnv(name: string) {
 }
 
 export const sanityWriteClient = createClient({
-  projectId: requireEnv("SANITY_PROJECT_ID"),
-  dataset: requireEnv("SANITY_DATASET"),
-  apiVersion: requireEnv("SANITY_API_VERSION"),
+  projectId: requireEnv("NEXT_PUBLIC_SANITY_PROJECT_ID"),
+  dataset: requireEnv("NEXT_PUBLIC_SANITY_DATASET"),
+  apiVersion: requireEnv("NEXT_PUBLIC_SANITY_API_VERSION"),
   token: requireEnv("SANITY_WRITE_TOKEN"),
   useCdn: false,
 });
