06d80922d9f89d0d8b1c690270edb1a108fd2d4a
Alexandr <AlexandorKsenz@gmail.com>
2026-03-08 20:33:39 +0100

    dataeventhome comp fixes

--- apps/web/src/features/home/ui/DataEventHome.tsx
+++ apps/web/src/features/home/ui/DataEventHome.tsx
@@ -14,19 +14,18 @@ export function DataEventHome({ title, buttonTitle, textDescription, gap = 0 }: 
       <h2 className="whitespace-pre-line text-gray mb-0 w-full text-[40px]/[111%] font-semibold max-w-[520px]">
         {title}
       </h2>
-      {buttonTitle ||
-        (textDescription && (
-          <div className="flex w-full max-w-[450px] flex-col gap-4">
-            {buttonTitle && (
-              <ButtonPrimary className="!w-[310px]" href={paths.events}>
-                {buttonTitle}
-              </ButtonPrimary>
-            )}
-            {textDescription && (
-              <p className="text-gray text-1xl/[128%] font-normal">{textDescription}</p>
-            )}
-          </div>
-        ))}
+      {(buttonTitle || textDescription) && (
+        <div className="flex w-full max-w-[450px] flex-col gap-4">
+          {buttonTitle && (
+            <ButtonPrimary className="!w-[310px]" href={paths.events}>
+              {buttonTitle}
+            </ButtonPrimary>
+          )}
+          {textDescription && (
+            <p className="text-gray text-1xl/[128%] font-normal">{textDescription}</p>
+          )}
+        </div>
+      )}
     </div>
   );
 }
