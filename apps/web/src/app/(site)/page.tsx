24a8960a46b859267d59b4f15cbeaba063e0cbe2
Alexandr <AlexandorKsenz@gmail.com>
2026-03-08 01:22:14 +0100

    added announcement section

--- apps/web/src/app/(site)/page.tsx
+++ apps/web/src/app/(site)/page.tsx
@@ -224,6 +224,70 @@ export default function HomePage() {
           </div>
         </Container>
       </section>
+      {/* ANNOUNCEMENT */}
+      <section className="relative bg-[#EEECE1] py-16 md:py-36">
+        <div className="absolute top-0 right-0 h-full w-[60%] max-[1440px]:w-[80%] max-[768px]:hidden">
+          <Image
+            src="/images/announcement.png"
+            alt="announcement"
+            aria-hidden="true"
+            width={400}
+            height={200}
+            className="object-left h-full w-full object-cover"
+          />
+        </div>
+        <Container variant="regular">
+          <div className="max-w-[400px]">
+            <h2 className="whitespace-pre-line text-gray mb-0 w-full text-[40px]/[111%] font-semibold">
+              Announcement of upcoming event
+            </h2>
+            <p className="py-8 text-1xl/[128%] font-normal">
+              The .ed Magazine is a carefully curated issue-based flagship digital publication of
+              the Global Citizenship Foundation.{" "}
+            </p>
+            {[
+              "The Stadios Institute, a non-profit enterprise is launched in Germany",
+              "Understanding of countries, communities, and cultures around the world",
+              "Mentor-Intern Program now caters to more organizations",
+            ].map((item, index) => {
+              return (
+                <div key={index} className="flex items-center gap-5 mb-10">
+                  <div className="w-20 h-20 shrink-0 bg-white rounded-full flex items-center justify-center">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="31"
+                      height="26"
+                      viewBox="0 0 31 26"
+                      fill="none"
+                    >
+                      <path
+                        d="M1.84564 16.5121C1.60173 16.4409 1.38747 16.2926 1.23503 16.0894C1.08258 15.8861 1.00017 15.6389 1.00017 15.3848V10.1006C1.00017 9.8465 1.08258 9.59928 1.23503 9.39602C1.38747 9.19276 1.60173 9.04442 1.84564 8.97328L27.6796 1.04697C27.8546 0.995927 28.039 0.98633 28.2184 1.01894C28.3978 1.05155 28.5671 1.12547 28.7129 1.23485C28.8587 1.34423 28.9771 1.48607 29.0586 1.64912C29.1402 1.81217 29.1826 1.99197 29.1826 2.17427V23.3111C29.1826 23.4934 29.1402 23.6732 29.0586 23.8363C28.9771 23.9993 28.8587 24.1411 28.7129 24.2505C28.5671 24.3599 28.3978 24.4338 28.2184 24.4664C28.039 24.499 27.8546 24.4894 27.6796 24.4384L1.84564 16.5121Z"
+                        stroke="#222222"
+                        strokeWidth="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      />
+                      <path
+                        d="M6.87138 18.0529V23.3107C6.87138 23.6221 6.9951 23.9208 7.21531 24.141C7.43553 24.3613 7.73421 24.485 8.04565 24.485H12.7427C13.0542 24.485 13.3528 24.3613 13.5731 24.141C13.7933 23.9208 13.917 23.6221 13.917 23.3107V5.26953"
+                        stroke="#222222"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      />
+                    </svg>
+                  </div>
+                  <p className="text-base text-gray font-medium">{item}</p>
+                </div>
+              );
+            })}
+            <div className="">
+              <ButtonPrimary href="#" className="!w-[310px]">
+                View the Magazine
+              </ButtonPrimary>
+            </div>
+          </div>
+        </Container>
+      </section>
       <section>
         <Newsletter />
       </section>
